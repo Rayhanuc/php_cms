9bb395c0c450bfcd6fd659e1b10407d72578dd07
Rayhanuc <rayhanctg4@gmail.com>
2023-11-04 20:44:31 +0600

    103. Adding Categories

--- admin/categories.php
+++ admin/categories.php
@@ -20,7 +20,25 @@
 
                     <!-- Add Category form start-->
                     <div class="col-xs-6">
-                        <form action="">
+                        <?php
+                            if (isset($_POST['submit'])) {
+                                $cat_title = $_POST['cat_title'];
+
+                                if ($cat_title == "" | empty($cat_title)) {
+                                    echo "<p class='alert-danger'>This field should not be empty.</p>";
+                                } else {
+                                    $query = "INSERT INTO categories(cat_title) ";
+                                    $query .= "VALUE('{$cat_title}')";
+
+                                    $create_category_query = mysqli_query($connection, $query);
+
+                                    if (!$create_category_query) {
+                                        die('QUERY FAILED' . mysqli_error($connection));
+                                    }
+                                }
+                            }
+                        ?>
+                        <form action="" method="post">
                             <div class="form-group">
                                 <label for="cat-title">Add Category</label>
                                 <input class="form-control" type="text" name="cat_title">
